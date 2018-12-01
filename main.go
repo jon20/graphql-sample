ebc850962ead6fe223c6496194400b266a93d8a0
jon <kawadu1221@gmail.com>
2018-12-02 03:49:01 +0900

    add host setting

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"github.com/jon20/graphql-sample/graphql"
+	"github.com/labstack/echo"
+)
 
 func main() {
 	route := echo.New()
 
-	route.POST("/graphql", echo.WrapHandler())
-	route.GET("/graphql", echo.WrapHandler())
+	route.POST("/graphql", echo.WrapHandler(graphql.GraphqlSetting()))
+	route.GET("/graphql", echo.WrapHandler(graphql.GraphqlSetting()))
+
+	route.Logger.Fatal(route.Start("localhost:8080"))
 }
